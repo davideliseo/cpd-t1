be11e11022438f79861a26753e81c58409911158
David Uribe <daviduribe998@gmail.com>
2022-09-01 22:29:14 -0400

    Fix derivación de monomios grado 0

--- app/src/monomio.cpp
+++ app/src/monomio.cpp
@@ -7,5 +7,10 @@ Monomio::Monomio(coeficiente_t coeficiente, grado_t grado)
 
 Monomio Monomio::derivado() const
 {
+    if (this->grado == 0)
+    {
+        return Monomio(0.0, 0);
+    }
+
     return Monomio(this->coeficiente * this->grado, this->grado - 1);
 }
